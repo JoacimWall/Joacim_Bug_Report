31c28768b5835eb734687b72f55c82d2b50e8050
joacimwall <joacim.wall@tietoevry.com>
2021-11-11 08:08:01 +0100

    Error report

---
+++ VideoError/VideoError/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VideoError.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
